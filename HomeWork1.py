729ad9d71058152ea9fe3decf527fc1e9c385f22
dus2171 <36135041+dus2171@users.noreply.github.com>
2018-02-11 14:06:53 +0200

    Add files via upload

--- HomeWork1.py
+++ HomeWork1.py
@@ -1,10 +1,16 @@
+# function that calculates the Sum of the list
+
 listValues = [4, 2, 7, 8, 9, 10, 2, 8, 6]
 listSum = sum(listValues)
 print(listSum)
 
+# function that calculates the Average of the list
+
 listLength = len(listValues)
 listAverage = listSum / listLength
 print(listAverage)
+
+# function that counts the vowel letters (a,e,i,o,u) in a given string
 
 count = 0
 string = "Type a sentence and I will count the vowels!".lower()
@@ -12,6 +18,8 @@ for char in string:
     if char in 'aeiou':
         count += 1
 print(count)
+
+# program that receives Name of user and his age, then prints the Name and on which year he will reach the age of 120
 
 name = input("What is your name: ")
 age = int(input("How old are you: "))
